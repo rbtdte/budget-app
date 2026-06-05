87be00dcc61b618bdc95b7986c9375ebf65c605d
robertdollete <dolle010@csusm.edu>
2026-06-04 21:09:59 -0700

    remove next-pwa from config

--- next.config.js
+++ next.config.js
@@ -1,28 +1,8 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'budget-app-cache',
-        networkTimeoutSeconds: 10,
-        expiration: {
-          maxEntries: 200,
-          maxAgeSeconds: 24 * 60 * 60, // 24h
-        },
-      },
-    },
-  ],
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  turbopack: {},
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
